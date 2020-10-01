a92e25cdd64e2a9435b001a500f46f83a87f2b28
Arfara Yema S <71955832+ArfaraYema@users.noreply.github.com>
2020-10-01 14:51:29 +0700

    Add files via upload

---
+++ tugassea.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //langsung saja menuliskan perintah di printf
+    printf("SELAMAT DATANG DI SEA CORP\n25 SEPTEMBER 2020\nCUACA CERAH\n\a");
+
+    return 0;
+}
